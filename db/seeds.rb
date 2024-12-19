1b24b173760a9b056488f8021b8cc59cfcebba50
Moises Almeida <moisesssprg@gmail.com>
2024-12-19 13:43:01 -0300

    adiciona tags ao seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,27 +13,32 @@ articles_data = [
   {
     title: "Benefícios da Yoga",
     content: "Yoga traz diversos benefícios, incluindo melhora da flexibilidade, redução do estresse e aumento da força mental. Praticar yoga regularmente pode transformar sua vida de maneira significativa.",
-    cover_image_url: "https://picsum.photos/800/600?random=1"
+    cover_image_url: "https://picsum.photos/800/600?random=1",
+    tags: "saúde, bem-estar, yoga"
   },
   {
     title: "Como viajar pode fazer bem para a saúde",
     content: "Viajar não é apenas um prazer, mas também uma oportunidade de cuidar da saúde mental e física. Explorar novos lugares ajuda a reduzir o estresse, melhora a criatividade e proporciona novas perspectivas.",
-    cover_image_url: "https://picsum.photos/800/600?random=2"
+    cover_image_url: "https://picsum.photos/800/600?random=2",
+    tags: "viagem, saúde, exploração"
   },
   {
     title: "Empreender para transformar o mundo em um lugar melhor",
     content: "Empreender é mais do que criar negócios. É uma forma de gerar impacto positivo, resolvendo problemas reais e contribuindo para uma sociedade mais justa e sustentável.",
-    cover_image_url: "https://picsum.photos/800/600?random=3"
+    cover_image_url: "https://picsum.photos/800/600?random=3",
+    tags: "empreendedorismo, impacto social, inovação"
   },
   {
     title: "Tecnologia como meio para empoderar as pessoas",
     content: "A tecnologia tem o poder de empoderar indivíduos e comunidades, oferecendo ferramentas para aprendizado, comunicação e inovação. Quando usada com responsabilidade, pode transformar vidas.",
-    cover_image_url: "https://picsum.photos/800/600?random=4"
+    cover_image_url: "https://picsum.photos/800/600?random=4",
+    tags: "tecnologia, inovação, empoderamento"
   },
   {
     title: "Como foi a viagem para Recife!",
     content: "Recife é um destino incrível, cheio de cultura, gastronomia e belezas naturais. Durante a viagem, conheci praias paradisíacas, explorei o centro histórico e provei pratos típicos inesquecíveis.",
-    cover_image_url: "https://picsum.photos/800/600?random=5"
+    cover_image_url: "https://picsum.photos/800/600?random=5",
+    tags: "viagem, Recife, gastronomia"
   }
 ]
 
@@ -41,6 +46,7 @@ articles_data.each do |article_data|
   article = Article.new(
     title: article_data[:title],
     content: article_data[:content],
+    tags: article_data[:tags],
   )
 
   # Adicionar imagem cover
